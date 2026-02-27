9bd1b66269dc156809f00b33075dc6345e173c53
Cyanogenoid <cosimo.giovanni@outlook.it>
2026-02-27 22:19:53 +0100

    hard cache fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: {
     host: true,
     port: 80,
   },
+  plugins: [
+    VitePWA({
+      selfDestroying: true,
+    })
+  ]
 });
